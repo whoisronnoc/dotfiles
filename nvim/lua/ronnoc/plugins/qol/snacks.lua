202098f2fafa48323d2dbe61c17b0b63414973a9
Connor Temple <connor@ronnoc.info>
2025-02-21 12:52:19 -0600

    snacks

---
+++ nvim/lua/ronnoc/plugins/qol/snacks.lua
@@ -0,0 +1,12 @@
+--- @return LazyPluginSpec
+return {
+	"folke/snacks.nvim",
+	---@type snacks.config
+	opts = {
+		terminal = {
+			-- your terminal configuration comes here
+			-- or leave it empty to use the default settings
+			-- refer to the configuration section below
+		},
+	},
+}
