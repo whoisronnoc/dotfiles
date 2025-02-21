1ab0b25491918ca39f11429798ed1f0bd6511ea2
Connor Temple <connor@ronnoc.info>
2025-02-21 13:19:17 -0600

    snacks

--- nvim/lua/ronnoc/plugins/qol/snacks.lua
+++ nvim/lua/ronnoc/plugins/qol/snacks.lua
@@ -3,10 +3,25 @@ return {
 	"folke/snacks.nvim",
 	---@type snacks.config
 	opts = {
-		terminal = {
-			-- your terminal configuration comes here
-			-- or leave it empty to use the default settings
-			-- refer to the configuration section below
+		image = {},
+		terminal = {},
+		toggle = {},
+		scratch = {},
+	},
+	keys = {
+		{
+			"<leader>.",
+			function()
+				Snacks.scratch()
+			end,
+			desc = "Toggle Scratch Buffer",
+		},
+		{
+			"<leader>S",
+			function()
+				Snacks.scratch.select()
+			end,
+			desc = "Select Scratch Buffer",
 		},
 	},
 }
