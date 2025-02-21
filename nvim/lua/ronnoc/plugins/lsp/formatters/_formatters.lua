fec318ef2d817fcc0a9e6b9f7a022af77ad61dc9
Connor Temple <connor@ronnoc.info>
2025-02-21 11:51:51 -0600

    fix vue config

--- nvim/lua/ronnoc/plugins/lsp/formatters/_formatters.lua
+++ nvim/lua/ronnoc/plugins/lsp/formatters/_formatters.lua
@@ -1,14 +1,15 @@
 -- map filetypes to formatters for conform.nvim
 return {
-	javascript = { "prettier" },
-	typescript = { "prettier" },
-	javascriptreact = { "prettier" },
-	typescriptreact = { "prettier" },
-	css = { "prettier" },
-	html = { "prettier" },
-	json = { "prettier" },
-	yaml = { "prettier" },
-	markdown = { "prettier" },
-	lua = { "stylua" },
-	python = { "isort", "black" },
+    vue = {"prettier"},
+    javascript = {"prettier"},
+    typescript = {"prettier"},
+    javascriptreact = {"prettier"},
+    typescriptreact = {"prettier"},
+    css = {"prettier"},
+    html = {"prettier"},
+    json = {"prettier"},
+    yaml = {"prettier"},
+    markdown = {"prettier"},
+    lua = {"stylua"},
+    python = {"isort", "black"}
 }
--- nvim/lua/ronnoc/plugins/lsp/servers/ts_ls.lua
+++ nvim/lua/ronnoc/plugins/lsp/servers/ts_ls.lua
@@ -1,4 +1,9 @@
 -- ts_ls
 return {
+	init_options = {
+		typescript = {
+			tsdk = vim.fn.getcwd() .. "/node_modules/typescript/lib",
+		},
+	},
 	filetypes = { "typescript", "javascript", "javascriptreact", "typescriptreact" },
 }
