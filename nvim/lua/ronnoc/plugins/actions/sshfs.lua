509089f210a1d72a414afa7df9e0657dc2ab6e29
whoisronnoc <connor@ronnoc.info>
2025-02-24 18:17:56 -0600

    sshfs plugin

---
+++ nvim/lua/ronnoc/plugins/actions/sshfs.lua
@@ -0,0 +1,10 @@
+-- https://github.com/nosduco/remote-sshfs.nvim
+-- :RemoteSSHFSConnect <[user@]host>:/path -p <port>
+return {
+	"nosduco/remote-sshfs.nvim",
+	dependencies = { "nvim-telescope/telescope.nvim" },
+	opts = {
+		-- Refer to the configuration section below
+		-- or leave empty for defaults
+	},
+}
--- nvim/lua/ronnoc/plugins/qol/auto-session.lua
+++ nvim/lua/ronnoc/plugins/qol/auto-session.lua
@@ -12,7 +12,7 @@ return {
 		-- The following are already the default values, no need to provide them if these are already the settings you want.
 		session_lens = {
 			-- If load_on_setup is false, make sure you use `:SessionSearch` to open the picker as it will initialize everything first
-			load_on_setup = true,
+			load_on_setup = false,
 			previewer = false,
 			mappings = {
 				-- Mode can be a string or a table, e.g. {"i", "n"} for both insert and normal mode
