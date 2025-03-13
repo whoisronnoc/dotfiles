f15b5985ae27d8e025f805a40f03be0ae044075b
Connor Temple <connor@ronnoc.info>
2025-03-13 14:51:05 -0500

    auto-dark-mode.nvim

--- nvim/lua/ronnoc/plugins/theme/theme.lua
+++ nvim/lua/ronnoc/plugins/theme/theme.lua
@@ -12,4 +12,22 @@ return {
 			vim.cmd([[colorscheme monokai-pro]])
 		end,
 	},
+	{
+		"f-person/auto-dark-mode.nvim",
+		opts = {
+			set_dark_mode = function()
+				-- vim.api.nvim_set_option_value("background", "dark", {})
+				vim.cmd([[colorscheme monokai-pro-spectrum]])
+			end,
+			set_light_mode = function()
+				-- vim.api.nvim_set_option_value("background", "light", {})
+				vim.cmd([[colorscheme monokai-pro-light]])
+			end,
+			update_interval = 3000,
+			fallback = "dark",
+			-- your configuration comes here
+			-- or leave it empty to use the default settings
+			-- refer to the configuration section below
+		},
+	},
 }
