64652c10119638a252ad18e4a2337a012ba72387
whoisronnoc <connor@ronnoc.info>
2025-03-13 20:40:47 -0500

    testing another colorscheme

--- nvim/lua/ronnoc/plugins/theme/theme.lua
+++ nvim/lua/ronnoc/plugins/theme/theme.lua
@@ -13,21 +13,22 @@ return {
 		end,
 	},
 	{
+		"NLKNguyen/papercolor-theme",
+		lazy = true,
+	},
+	{
 		"f-person/auto-dark-mode.nvim",
 		opts = {
 			set_dark_mode = function()
-				-- vim.api.nvim_set_option_value("background", "dark", {})
+				vim.api.nvim_set_option_value("background", "dark", {})
 				vim.cmd([[colorscheme monokai-pro-spectrum]])
 			end,
 			set_light_mode = function()
-				-- vim.api.nvim_set_option_value("background", "light", {})
-				vim.cmd([[colorscheme monokai-pro-light]])
+				vim.api.nvim_set_option_value("background", "light", {})
+				vim.cmd([[colorscheme PaperColor]])
 			end,
 			update_interval = 3000,
 			fallback = "dark",
-			-- your configuration comes here
-			-- or leave it empty to use the default settings
-			-- refer to the configuration section below
 		},
 	},
 }
