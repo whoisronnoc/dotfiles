c710fbd1a09b6f21c97e72f6dc6d0802ac60b959
whoisronnoc <connor@ronnoc.info>
2025-03-23 20:32:11 -0500

    keymap descriptions

--- nvim/lua/ronnoc/plugins/actions/session.lua
+++ nvim/lua/ronnoc/plugins/actions/session.lua
@@ -3,34 +3,53 @@ return {
 	event = "BufReadPre", -- this will only start session saving when an actual file was opened
 	opts = {
 		-- add any custom options here
-
 		dir = vim.fn.stdpath("state") .. "/sessions/", -- directory where session files are saved
 		-- minimum number of file buffers that need to be open to save
 		-- Set to 0 to always save
-		-- need = 1,
-		-- branch = true, -- use git branch to save session
+		need = 0,
+		branch = false, -- use git branch to save session
+		options = { "buffers", "curdir", "tabpages", "winsize" }, -- session save options
 	},
 	config = function(_, opts)
 		require("persistence").setup(opts)
 		-- load the session for the current directory
 		vim.keymap.set("n", "<leader>qs", function()
 			require("persistence").load()
-		end)
+		end, { desc = "[s]elect current session" })
 
 		-- select a session to load
 		vim.keymap.set("n", "<leader>qS", function()
 			require("persistence").select()
-		end)
+		end, { desc = "[S]elect session" })
 
 		-- load the last session
 		vim.keymap.set("n", "<leader>ql", function()
 			require("persistence").load({ last = true })
-		end)
+		end, { desc = "[l]oad last session" })
 
 		-- stop Persistence => session won't be saved on exit
 		vim.keymap.set("n", "<leader>qd", function()
 			require("persistence").stop()
-		end)
+		end, { desc = "[d]elete session" })
+
+		-- Create an autocommand to load session when starting Neovim in a directory
+		vim.api.nvim_create_autocmd("VimEnter", {
+			callback = function()
+				-- Only try to load if no arguments were passed (just opening a directory)
+				-- and not in a git commit, merge, or similar scenario
+				local should_load = vim.fn.argc() == 0
+					and vim.fn.line2byte(1) == -1
+					and not vim.o.insertmode
+					and vim.env.TERM ~= "dumb"
+					and not vim.env.GIT_EXEC_PATH
+
+				if should_load then
+					-- Try to load the session for the current directory
+					require("persistence").load({ last = false })
+				end
+			end,
+			desc = "Auto-load session when opening Neovim in a directory",
+		})
 	end,
 	-- keys = {
 	-- 	{ "<leader>qs", require("persistence").load, desc = "Restore Session" },
