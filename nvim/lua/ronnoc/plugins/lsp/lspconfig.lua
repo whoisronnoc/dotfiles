98a59a0dceffc4f3ab393f57235f682ce5a7872a
Connor Temple <connor@ronnoc.info>
2025-03-25 15:12:19 -0500

    dap

--- nvim/lua/ronnoc/plugins/lsp/lspconfig.lua
+++ nvim/lua/ronnoc/plugins/lsp/lspconfig.lua
@@ -55,7 +55,18 @@ return {
 			"b0o/schemastore.nvim",
 		},
 		config = function()
-			vim.diagnostic.config({ update_in_insert = true })
+			vim.fn.sign_define("DiagnosticSignError", { text = "", texthl = "DiagnosticSignError" })
+			vim.fn.sign_define("DiagnosticSignWarn", { text = "", texthl = "DiagnosticSignWarn" })
+			vim.fn.sign_define("DiagnosticSignInfo", { text = "", texthl = "DiagnosticSignInfo" })
+			vim.fn.sign_define("DiagnosticSignHint", { text = "", texthl = "DiagnosticSignHint" })
+
+			vim.diagnostic.config({
+				update_in_insert = false,
+				virtual_text = false,
+				signs = true,
+				underline = true,
+				severity_sort = true,
+			})
 
 			vim.api.nvim_create_autocmd("LspAttach", {
 				group = vim.api.nvim_create_augroup("kickstart-lsp-attach", {
@@ -140,5 +151,42 @@ return {
 				lspconfig[server_name].setup(config)
 			end
 		end,
+	},
+	{
+		"folke/trouble.nvim",
+		opts = {}, -- for default options, refer to the configuration section for custom setup.
+		cmd = "Trouble",
+		keys = {
+			{
+				"<leader>xx",
+				"<cmd>Trouble diagnostics toggle<cr>",
+				desc = "Diagnostics (Trouble)",
+			},
+			{
+				"<leader>xX",
+				"<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
+				desc = "Buffer Diagnostics (Trouble)",
+			},
+			{
+				"<leader>cs",
+				"<cmd>Trouble symbols toggle focus=false<cr>",
+				desc = "Symbols (Trouble)",
+			},
+			{
+				"<leader>cl",
+				"<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
+				desc = "LSP Definitions / references / ... (Trouble)",
+			},
+			{
+				"<leader>xL",
+				"<cmd>Trouble loclist toggle<cr>",
+				desc = "Location List (Trouble)",
+			},
+			{
+				"<leader>xQ",
+				"<cmd>Trouble qflist toggle<cr>",
+				desc = "Quickfix List (Trouble)",
+			},
+		},
 	},
 }
