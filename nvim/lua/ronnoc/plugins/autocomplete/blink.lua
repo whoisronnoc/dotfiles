856b93f18bb88188daa3101d3f85ca9386dd6dc5
whoisronnoc <connor@ronnoc.info>
2025-03-25 06:43:03 -0500

    blink colorful-menu

--- nvim/lua/ronnoc/plugins/autocomplete/blink.lua
+++ nvim/lua/ronnoc/plugins/autocomplete/blink.lua
@@ -159,13 +159,16 @@ return {
 					-- auto_show = true,
 					draw = {
 						columns = { { "kind_icon" }, { "label", gap = 1 } },
-
-						-- components = {
-						-- 	label = {
-						-- 		text = require("colorful-menu").blink_components_text,
-						-- 		highlight = require("colorful-menu").blink_components_highlight,
-						-- 	},
-						-- },
+						components = {
+							label = {
+								text = function(ctx)
+									return require("colorful-menu").blink_components_text(ctx)
+								end,
+								highlight = function(ctx)
+									return require("colorful-menu").blink_components_highlight(ctx)
+								end,
+							},
+						},
 					},
 					border = border,
 				},
