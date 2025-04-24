72a97e98b07988a0c7c2026e7ad56948237e7139
Connor Temple <connor@ronnoc.info>
2025-04-24 08:24:01 -0500

    update ibl colors

--- nvim/lua/plugins/theme/indentline.lua
+++ nvim/lua/plugins/theme/indentline.lua
@@ -3,18 +3,29 @@ return {
 	main = "ibl",
 	event = { "BufReadPost", "BufWritePost", "BufNewFile" },
 	opts = function(_, opts)
-		return require("indent-rainbowline").make_opts(opts, {
-			exclude = { filetypes = { "dashboard", "text" } },
-			colors = {
-				-- 0xff6e67,
-				-- 0xff8700,
-				-- 0x5ffa68,
-				0xfffa67,
-				-- 0x6871ff,
-				0xff77ff,
-				0x60fdff,
+		vim.cmd.hi("IblScope guifg=#aaaaaa")
+		return require("indent-rainbowline").make_opts(
+			-- ibl opts
+			{
+				scope = {
+					char = "▎",
+					-- inject_languages = false,
+				},
 			},
-		})
+			-- rainbow opts
+			{
+				exclude = { filetypes = { "dashboard", "text" } },
+				colors = {
+					-- 0xff6e67,
+					0xff8700,
+					-- 0x5ffa68,
+					-- 0xfffa67,
+					-- 0x6871ff,
+					-- 0xff77ff,
+					0x60fdff,
+				},
+			}
+		)
 	end,
 	dependencies = {
 		"TheGLander/indent-rainbowline.nvim",
