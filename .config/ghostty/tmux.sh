9c3d9f3227c6131fb7731f6f539ce4a06faea57d
whoisronnoc <connor@ronnoc.info>
2025-10-24 20:06:54 -0500

    fix ghostty tmux command

--- .config/ghostty/tmux.sh
+++ .config/ghostty/tmux.sh
@@ -3,13 +3,13 @@
 SESSION_NAME="ghostty"
 
 # Check if the session already exists
-/usr/local/bin/tmux has-session -t $SESSION_NAME 2>/dev/null
+tmux has-session -t $SESSION_NAME 2>/dev/null
 
 if [ $? -eq 0 ]; then
   # If the session exists, reattach to it
-  /usr/local/bin/tmux attach-session -t $SESSION_NAME
+  tmux attach-session -t $SESSION_NAME
 else
   # If the session doesn't exist, start a new one
-  /usr/local/bin/tmux new-session -s $SESSION_NAME -d
-  /usr/local/bin/tmux attach-session -t $SESSION_NAME
+  tmux new-session -s $SESSION_NAME -d
+  tmux attach-session -t $SESSION_NAME
 fi
