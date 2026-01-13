c47d00ff0a9d51d5802383dbc85e33ea036bb2cf
Connor Temple <connor@ronnoc.info>
2026-01-13 11:40:18 -0600

    enable sidekick keys

--- .config/nvim/lua/plugins/autocomplete/sidekick.lua
+++ .config/nvim/lua/plugins/autocomplete/sidekick.lua
@@ -28,68 +28,68 @@ return {
 				expr = true,
 				desc = "Goto/Apply Next Edit Suggestion",
 			},
-			-- {
-			-- 	"<c-.>",
-			-- 	function()
-			-- 		require("sidekick.cli").toggle()
-			-- 	end,
-			-- 	desc = "Sidekick Toggle",
-			-- 	mode = { "n", "t", "i", "x" },
-			-- },
-			-- {
-			-- 	"<leader>aa",
-			-- 	function()
-			-- 		require("sidekick.cli").toggle()
-			-- 	end,
-			-- 	desc = "Sidekick Toggle CLI",
-			-- },
-			-- {
-			-- 	"<leader>as",
-			-- 	function()
-			-- 		require("sidekick.cli").select()
-			-- 	end,
-			-- 	-- Or to select only installed tools:
-			-- 	-- require("sidekick.cli").select({ filter = { installed = true } })
-			-- 	desc = "Select CLI",
-			-- },
-			-- {
-			-- 	"<leader>ad",
-			-- 	function()
-			-- 		require("sidekick.cli").close()
-			-- 	end,
-			-- 	desc = "Detach a CLI Session",
-			-- },
-			-- {
-			-- 	"<leader>at",
-			-- 	function()
-			-- 		require("sidekick.cli").send({ msg = "{this}" })
-			-- 	end,
-			-- 	mode = { "x", "n" },
-			-- 	desc = "Send This",
-			-- },
-			-- {
-			-- 	"<leader>af",
-			-- 	function()
-			-- 		require("sidekick.cli").send({ msg = "{file}" })
-			-- 	end,
-			-- 	desc = "Send File",
-			-- },
-			-- {
-			-- 	"<leader>av",
-			-- 	function()
-			-- 		require("sidekick.cli").send({ msg = "{selection}" })
-			-- 	end,
-			-- 	mode = { "x" },
-			-- 	desc = "Send Visual Selection",
-			-- },
-			-- {
-			-- 	"<leader>ap",
-			-- 	function()
-			-- 		require("sidekick.cli").prompt()
-			-- 	end,
-			-- 	mode = { "n", "x" },
-			-- 	desc = "Sidekick Select Prompt",
-			-- },
+			{
+				"<leader>kt",
+				function()
+					require("sidekick.cli").toggle()
+				end,
+				desc = "Sidekick Toggle",
+				mode = { "n", "t", "i", "x" },
+			},
+			{
+				"<leader>ki",
+				function()
+					require("sidekick.cli").toggle()
+				end,
+				desc = "Sidekick Toggle CLI",
+			},
+			{
+				"<leader>ks",
+				function()
+					require("sidekick.cli").select()
+				end,
+				-- Or to select only installed tools:
+				-- require("sidekick.cli").select({ filter = { installed = true } })
+				desc = "Select CLI",
+			},
+			{
+				"<leader>kd",
+				function()
+					require("sidekick.cli").close()
+				end,
+				desc = "Detach a CLI Session",
+			},
+			{
+				"<leader>kt",
+				function()
+					require("sidekick.cli").send({ msg = "{this}" })
+				end,
+				mode = { "x", "n" },
+				desc = "Send This",
+			},
+			{
+				"<leader>kf",
+				function()
+					require("sidekick.cli").send({ msg = "{file}" })
+				end,
+				desc = "Send File",
+			},
+			{
+				"<leader>kv",
+				function()
+					require("sidekick.cli").send({ msg = "{selection}" })
+				end,
+				mode = { "x" },
+				desc = "Send Visual Selection",
+			},
+			{
+				"<leader>kp",
+				function()
+					require("sidekick.cli").prompt()
+				end,
+				mode = { "n", "x" },
+				desc = "Sidekick Select Prompt",
+			},
 		},
 	},
 	-- {
