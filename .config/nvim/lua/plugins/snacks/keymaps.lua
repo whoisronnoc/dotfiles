f16b2d757114973c39a0810dacd16317bc3fcccd
whoisronnoc <connor@ronnoc.info>
2026-01-15 12:42:55 -0600

    snacks

--- .config/nvim/lua/plugins/snacks/keymaps.lua
+++ .config/nvim/lua/plugins/snacks/keymaps.lua
@@ -5,9 +5,9 @@ return {
 	optional = true,
 	--- @module 'snacks'
 	--- @type snacks.Config
-  -- stylua: ignore
+	-- stylua: ignore
 	keys = { 
-  	{ "<leader>z",  function() Snacks.zen() end, desc = "Toggle Zen Mode" },
+		{ "<leader>z",  function() Snacks.zen() end, desc = "Toggle Zen Mode" },
 		{ "<leader>Z",  function() Snacks.zen.zoom() end, desc = "Toggle Zoom" },
 		{ "<leader>.",  function() Snacks.scratch() end, desc = "Toggle Scratch Buffer" },
 		{ "<leader>S",  function() Snacks.scratch.select() end, desc = "Select Scratch Buffer" },
@@ -35,34 +35,3 @@ return {
 		end }
 	},
 }
-
---      -- Create some toggle mappings
---      -- stylua: ignore start
---      -- Snacks.toggle.option("spell", { name = "[s]pelling" }):map("<leader>ts")
---      Snacks.toggle.option("wrap", { name = "[w]rap" }):map("<leader>tw")
---      Snacks.toggle.option("conceallevel", { off = 0, on = vim.o.conceallevel > 0 and vim.o.conceallevel or 2 }):map("<leader>tc")
---      Snacks.toggle.option("mousescroll", { off = "ver:3,hor:0", on = "ver3,hor:1", name = "Horizontal Scroll" }):map("<leader>tm")
---
---      Snacks.toggle.line_number():map("<leader>tl")
---      Snacks.toggle.option("relativenumber", { name = "Relative Number" }):map("<leader>tL")
---
---      Snacks.toggle.diagnostics():map("<leader>td")
---      Snacks.toggle.treesitter():map("<leader>tT")
---      Snacks.toggle.inlay_hints():map("<leader>th")
---
---      Snacks.toggle.option("background", { off = "light", on = "dark", name = "Dark [b]ackground" }):map("<leader>tb")
--- 	-- stylua: ignore end
---
--- 	Snacks.toggle
--- 		.new({
--- 			id = "toggle_autoformat",
--- 			name = "Auto [f]ormat",
--- 			get = function()
--- 				return vim.g.autoformat
--- 			end,
--- 			set = function(state)
--- 				vim.g.autoformat = state
--- 			end,
--- 		})
--- 		:map("<leader>tf")
--- end,
---
+++ .config/nvim/lua/plugins/snacks/toggle.lua
@@ -0,0 +1,44 @@
+---@module "lazy"
+---@type LazyPluginSpec
+return {
+	"folke/snacks.nvim",
+	optional = true,
+	--- @module 'snacks'
+	--- @type snacks.Config
+	-- stylua: ignore
+	init = function()
+		vim.api.nvim_create_autocmd("User", {
+			pattern = "VeryLazy",
+			callback = function()
+				-- Create some toggle mappings
+				-- stylua: ignore start
+				-- Snacks.toggle.option("spell", { name = "[s]pelling" }):map("<leader>ts")
+				Snacks.toggle.option("wrap", { name = "[w]rap" }):map("<leader>tw")
+				Snacks.toggle.option("conceallevel", { off = 0, on = vim.o.conceallevel > 0 and vim.o.conceallevel or 2 }):map("<leader>tc")
+				Snacks.toggle.option("mousescroll", { off = "ver:3,hor:0", on = "ver3,hor:1", name = "Horizontal Scroll" }):map("<leader>tm")
+
+				Snacks.toggle.line_number():map("<leader>tl")
+				Snacks.toggle.option("relativenumber", { name = "Relative Number" }):map("<leader>tL")
+
+				Snacks.toggle.diagnostics():map("<leader>td")
+				Snacks.toggle.treesitter():map("<leader>tT")
+				Snacks.toggle.inlay_hints():map("<leader>th")
+
+				Snacks.toggle.option("background", { off = "light", on = "dark", name = "Dark [b]ackground" }):map("<leader>tb")
+				-- stylua: ignore end
+				Snacks.toggle
+					.new({
+						id = "toggle_autoformat",
+						name = "Auto [f]ormat",
+						get = function()
+							return vim.g.autoformat
+						end,
+						set = function(state)
+							vim.g.autoformat = state
+						end,
+					})
+					:map("<leader>tf")
+			end,
+		})
+	end,
+}
